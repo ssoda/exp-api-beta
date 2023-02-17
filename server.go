293c63e3e58388356918e1a65129ab07e072e094
ssoda <demaciasoda@gmail.com>
2023-02-17 12:35:05 +0800

    [Added] set up first testing

--- server.go
+++ server.go
@@ -7,12 +7,18 @@ import (
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
-
+	r := setupRouter()
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+	r.GET("/ping", PingHandler)
+	return r
+}
+
+func PingHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "pong",
+	})
 }
